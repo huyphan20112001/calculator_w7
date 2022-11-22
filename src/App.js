37bad9c3acb2d454b15f0a627e60fe60b07a3cf9
Phan Ba Huy <huy.pb.2011@gmail.com>
2022-11-22 20:31:28 +0700

    22112022

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { number } from "./constants/constants";
 
 function App() {
+  const [currentNumber, setCurrentNumber] = useState(0);
+  const [firstNumber, setFirstNumber] = useState(0);
+  const [operator, setOperator] = useState();
+
+  const inputCurrentNumber = (e) => {
+    let input = e.target.value;
+    if (currentNumber === 0) {
+      setCurrentNumber(input);
+    } else {
+      setCurrentNumber(currentNumber + input);
+    }
+  };
+
+  const clear = () => {
+    setCurrentNumber(0);
+  };
+
+  const porcentagem = (e) => {
+    setCurrentNumber(currentNumber / 100);
+  };
+
+  const changeSign = () => {
+    if (currentNumber > 0) {
+      setCurrentNumber(-currentNumber);
+    } else {
+      setCurrentNumber(Math.abs(currentNumber));
+    }
+  };
+
+  const operatorHandler = (e) => {
+    let operatorInput = e.target.value;
+    setOperator(operatorInput);
+    setFirstNumber(currentNumber);
+    setCurrentNumber(0);
+  };
+
+  const calculate = () => {
+    if (operator === "/") {
+      setCurrentNumber(parseFloat(firstNumber) / parseFloat(currentNumber));
+    } else if (operator === "X") {
+      setCurrentNumber(parseFloat(firstNumber) * parseFloat(currentNumber));
+    } else if (operator === "-") {
+      setCurrentNumber(parseFloat(firstNumber) - parseFloat(currentNumber));
+    } else if (operator === "+") {
+      setCurrentNumber(parseFloat(firstNumber) + parseFloat(currentNumber));
+    }
+  };
+
+  console.log(number.slice(-3));
+  console.log(number.slice(-6, -3));
+  console.log(number.slice(-9, -6));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="wrapper">
+        <h1 className="result">{currentNumber}</h1>
+        <button onClick={clear}>AC</button>
+        <button onClick={changeSign}>+/-</button>
+        <button onClick={porcentagem}>%</button>
+        <button className="orange" onClick={operatorHandler} value={"/"}>
+          /
+        </button>
+        {number.slice(-3).map((item) => (
+          <button className="grey" onClick={inputCurrentNumber} value={item.id}>
+            {item.id}
+          </button>
+        ))}
+
+        <button className="orange" onClick={operatorHandler} value={"X"}>
+          X
+        </button>
+        {number.slice(-6, -3).map((item) => (
+          <button className="grey" onClick={inputCurrentNumber} value={item.id}>
+            {item.id}
+          </button>
+        ))}
+
+        <button className="orange" onClick={operatorHandler} value={"-"}>
+          -
+        </button>
+        {number.slice(-9, -6).map((item) => (
+          <button className="grey" onClick={inputCurrentNumber} value={item.id}>
+            {item.id}
+          </button>
+        ))}
+
+        <button className="orange" onClick={operatorHandler} value={"+"}>
+          +
+        </button>
+        <button className="grey" onClick={inputCurrentNumber} value={0}>
+          0
+        </button>
+        <button style={{ visibility: "hidden" }}>k</button>
+        <button className="grey" onClick={inputCurrentNumber} value={"."}>
+          ,
+        </button>
+        <button className="orange" onClick={calculate}>
+          =
+        </button>
+      </div>
     </div>
   );
 }
